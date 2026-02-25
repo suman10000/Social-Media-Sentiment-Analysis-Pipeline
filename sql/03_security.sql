5ed5a350a9b8cda7299c3dddbc6e17835806e016
Suman Dandapat <sumandandapat554@gmail.com>
2026-02-25 19:41:55 +0530

    Added semi-structured JSON implementation

--- sql/03_security.sql
+++ sql/03_security.sql
@@ -27,3 +27,32 @@ FROM dim_user;
 
 USE ROLE SYSADMIN;
 SELECT * FROM dim_user_masked;
+
+
+--for semi structured 
+
+CREATE OR REPLACE TABLE tweet_raw_json (
+    id STRING,
+    tweet_data VARIANT
+);
+
+INSERT INTO tweet_raw_json (id, tweet_data)
+SELECT
+    '1',
+    PARSE_JSON('{
+        "user": "john_doe",
+        "location": "Mumbai",
+        "device": "Android",
+        "hashtags": ["#AI", "#BigData"],
+        "engagement": {
+            "likes": 120,
+            "retweets": 45
+        }
+    }');
+
+SELECT
+    tweet_data:user::STRING AS user_name,
+    tweet_data:location::STRING AS location,
+    tweet_data:engagement.likes::INTEGER AS likes,
+    tweet_data:hashtags[0]::STRING AS first_hashtag
+FROM tweet_raw_json;
